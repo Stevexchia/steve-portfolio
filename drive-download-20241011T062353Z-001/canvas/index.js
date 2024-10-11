50340fe6a13518e917169bb9ed777c322a1345d1
Stevexchia <stevexchia@gmail.com>
2024-10-11 17:40:15 +0800

    cleanup

--- drive-download-20241011T062353Z-001/canvas/index.js
+++
@@ -1,6 +0,0 @@
-import EarthCanvas from "./Earth";
-import BallCanvas from "./Ball";
-import ComputersCanvas from "../../src/components/Computers";
-import StarsCanvas from "./Stars";
-
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
